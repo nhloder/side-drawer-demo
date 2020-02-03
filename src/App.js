b262512705c0e65fae83902943266ef896d52e70
nhloder <nhloder@gmail.com>
2020-02-03 16:03:20 -0700

    one

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import Toolbar from "./components/Toolbar/Toolbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Toolbar />
+      <main style ={{marginTop: '56px'}}>
+        <p> This is some Page content</p>
+      </main>
     </div>
   );
 }
---
+++ src/components/Toolbar/Toolbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import drawerToggleButton from'../DrawerToggleButton'
+
+import './Toolbar.css'
+
+const Toolbar = props => (
+  <header className="toolbar">
+    <nav className="toolbar__navigation">
+      <div />
+      
+      <div className="toolbar__logo">
+        <a href="/">THE LOGO</a>
+      </div>
+      <div className="spacer" />
+      <div className="toolbar_navigation-items">
+        <ul>
+          <li>
+            <a href="/">Products</a>
+          </li>
+          <li>
+            <a href="/">Users</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+)
+
+export default Toolbar
